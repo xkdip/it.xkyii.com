26367c412acbec5ed252eff9b3675fc61d723e1e
xkyii <vkyii@foxmail.com>
2020-11-07 18:44:18 +0800

    Add：gen

---
+++ .gen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace WebGenerator
+{
+    public class Program
+    {
+        private static readonly NormalizedPath ArtifactsFolder = "artifacts";
+
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .ConfigureEngine(x =>
+                {
+                    x.FileSystem.RootPath = new NormalizedPath(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.Parent;
+                    x.FileSystem.OutputPath = x.FileSystem.RootPath / ArtifactsFolder;
+                    x.FileSystem.InputPaths.Clear();
+                    x.FileSystem.InputPaths.Add(x.FileSystem.RootPath);
+                })
+                .AddSetting(WebKeys.ExcludedPaths, new List<NormalizedPath>
+                {
+                    new NormalizedPath(".gen"),
+                    new NormalizedPath(".github"),
+                    new NormalizedPath(".git"),
+                })
+                .RunAsync();
+    }
+}
